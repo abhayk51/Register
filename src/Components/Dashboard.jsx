029b10d56b7792cbf41167fde532cba5afb93605
Abhay Katiyar <abhaykatiyar51@gmail.com>
2025-02-09 14:46:18 +0530

    eighth commit

--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -1,21 +1,37 @@
-import React, { useEffect } from 'react';
-import { redirect, useNavigate } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Dashboard = () => {
+  const [flag, setFlag] = useState(false);
+  const [label, setLabel] = useState('');
+  const [input, setInput] = useState('');
   const navigate = useNavigate();
   let list = JSON.parse(localStorage.getItem("List"));
-  // if(list){
-    //   return (list = JSON.parse(localStorage.getItem("List")));
-    // }else {
-  //   return [];
-  // }
-  if (list.length){
-    console.log(list[0].name);
-  }else{
+  
+  let editEmail = () => {
+    setFlag(true);
+    setLabel('Email');
+    setInput(list[0].email);
+  }
+
+  let editPhone = () => {
+    setFlag(true);
+    setLabel('Phone');
+    setInput(list[0].phone);
+  }
+
+  let editPass = () => {
+    setFlag(true);
+    setLabel('Password');
+    setInput(list[0].pass);
+  }
+
+  if (!list.length){
     useEffect(()=>{
       navigate("/");
     })
   }
+
   if(list.length){
   return (
     <>
@@ -28,16 +44,27 @@ const Dashboard = () => {
         </p>
         <p className='d-flex justify-content-between'>
           <span className=''>Email : <span className='text-success'>{list[0].email}</span></span>
-          <span className=''><button className='btn btn-sm btn-success'><i class="bi bi-pencil-square fs-5"></i></button></span>
+          <span className=''><button className='btn btn-sm btn-success' onClick={editEmail}><i class="bi bi-pencil-square fs-5"></i></button></span>
         </p>
         <p className='d-flex justify-content-between'>
           <span className=''>Phone no. : <span className='text-success'>{list[0].phone}</span></span>
-          <span className=''><button className='btn btn-sm btn-success'><i class="bi bi-pencil-square fs-5"></i></button></span>
+          <span className=''><button className='btn btn-sm btn-success' onClick={editPhone}><i class="bi bi-pencil-square fs-5"></i></button></span>
         </p>
         <p className='d-flex justify-content-between'>
           <span className=''>Password : <span className='text-success'>{list[0].pass}</span></span>
-          <span className=''><button className='btn btn-sm btn-success'><i class="bi bi-pencil-square fs-5"></i></button></span>
+          <span className=''><button className='btn btn-sm btn-success' onClick={editPass}><i class="bi bi-pencil-square fs-5"></i></button></span>
         </p>
+        {flag ? (
+        <form action="" className='d-flex'>
+          <label htmlFor="" className='form-label m-2'>{label}</label>
+          <input
+            type="text"
+            className='form-control m-2'
+            value={input}
+            onChange={(e)=> setInput(e.target.value)}
+          />
+          <input type="submit" className='btn btn-sm btn-success m-2' value={"Update"}/>
+        </form>) : null}
         </div>
       </div>
     </>
